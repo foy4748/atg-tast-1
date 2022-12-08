4b7a2dab8d0331cc2a3b3c92b48aedfcf9eb8ba6
unknown <faisaljfcl@gmail.com>
2022-12-08 12:04:45 +0600

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Banner />
-      <Card />
+      <Container>
+        <Card />
+      </Container>
     </>
   );
 }
